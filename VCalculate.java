627d79bc91fbb64150f194863d9d00ce2f60670c
Lars75 <lars75@inbox.ru>
2016-11-09 16:01:19 +0300

    create VCalculate

---
+++ VCalculate.java
@@ -0,0 +1,9 @@
+public class VCalculate{
+	public static void main(String[] arg) {
+		System.out.println("...Calculate...");
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int sub = first - second;
+		System.out.println("sub " + sub);
+	}
+}
