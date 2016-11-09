03579033aa8faa5ac98f148edbac9eeda4bfd6e8
Lars75 <lars75@inbox.ru>
2016-11-09 13:36:30 +0300

    create Calculate

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,6 @@ public class Calculate {
 	int first = Integer.valueOf(arg[0]);
 	int second = Integer.valueOf(arg[1]);
 	int summ = first + second;
-	System.out.println("sum " + summ);
+	System.out.println("summ  " + summ);
 	}
 }
